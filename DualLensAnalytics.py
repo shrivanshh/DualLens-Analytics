387374a14896fa8254bb225af43a484e1874cc95
Himanshu Shrivansh <shrivanshh@gmail.com>
2026-02-06 01:59:51 -0500

    Completed all steps, only last description pending

--- DualLensAnalytics.py
+++ DualLensAnalytics.py
@@ -1,6 +1,4 @@
 import DualLensAnalyticsUtil as DLAUtils
-from langchain_community.document_loaders import PyPDFDirectoryLoader, PyPDFLoader     # Loading a PDF
-from langchain_community.vectorstores import Chroma    # Vector DataBase
 
 # 1. Organization Selection
 companies = ["GOOGL", "MSFT", "IBM", "NVDA", "AMZN"]
@@ -22,8 +20,78 @@ DLAUtils.plot_stock_trends(companies)
 
 # 4. RAG-Driven Analysis
 # 4.A. Fetching Financial Metrics for the companies
-DLAUtils.fetch_financial_metrics(companies) # Fetching the financial metrics for the companies
-DLAUtils.load_ai_initiative_documents() # Loading the AI Initiative Documents
+df = DLAUtils.fetch_financial_metrics(companies) # Fetching the financial metrics for the companies
+ai_initiative_chunks = DLAUtils.load_ai_initiative_documents() # Loading the AI Initiative Documents
 
 #4.B. Vectorizing AI Initiative Documents with ChromaDB
+retriever, vectorstore = DLAUtils.vectorize_ai_initiatives(ai_initiative_chunks)
 
+# 4. C. C. Retrieving relevant Documents
+user_message = "Give me the best project that `IBM` company is working upon"
+
+# Building the context for the query using the retrieved chunks
+relevant_document_chunks = retriever.invoke(user_message)
+context_list = [d.page_content for d in relevant_document_chunks]
+context_for_query = ". ".join(context_list)
+
+len(relevant_document_chunks)
+
+# Write a system message for an LLM to help craft a response from the provided context
+qna_system_message = """
+You are a helpful assistant for answering questions related to the AI initiatives of top tech companies."""
+
+# Write an user message template which can be used to attach the context and the questions
+qna_user_message_template = """
+###Context
+Here are some documents that are relevant to the question mentioned below.
+{context}
+
+###Question
+{question}
+"""
+
+# Format the prompt
+formatted_prompt = f"""[INST]{qna_system_message}\n
+                {'user'}: {qna_user_message_template.format(context=context_for_query, question=user_message)}
+                [/INST]"""
+
+# Make the LLM call
+resp = llm.invoke(formatted_prompt)
+resp.content
+
+
+# Test Cases
+print(DLAUtils.RAG("How is the area in which GOOGL is working different from the area in which MSFT is working?", qna_system_message, qna_user_message_template, retriever, llm))
+
+print(DLAUtils.RAG("What are the three projects on which MSFT is working upon?", qna_system_message, qna_user_message_template, retriever, llm))
+
+print(DLAUtils.RAG("What is the timeline of each project in NVDA?", qna_system_message, qna_user_message_template, retriever, llm))
+
+print(DLAUtils.RAG("What are the areas in which AMZN is investing when it comes to AI?", qna_system_message, qna_user_message_template, retriever, llm))
+
+print(DLAUtils.RAG("What are the risks associated with projects within GOOG?", qna_system_message, qna_user_message_template, retriever, llm))
+
+# 4. D. Evaluation of the RAG
+print(DLAUtils.evaluate_groundedness("What are the three projects on which MSFT is working upon?", qna_system_message, qna_user_message_template, retriever, resp.content, qna_user_message_template, llm))
+
+print(DLAUtils.evaluate_relevance("What are the three projects on which MSFT is working upon?", qna_system_message, qna_user_message_template, retriever, resp.content, qna_user_message_template, llm))
+
+# 5. Scoring and Ranking
+print (DLAUtils.score_and_rank_companies(vectorstore, llm, df))
+
+# 6. Summary and Future Scope
+'''TODO:
+Based on the project, learners are expected to share their observations, key learnings, and insights related to the business use case, including any challenges they encountered. 
+Additionally, they should recommend improvements to the project and suggest further steps for enhancement.
+
+A. Summary / Your Observations about this Project - 2 Marks
+1.
+2. 
+3. 
+
+B. Future scope of this Project - 2 Marks
+1.
+2.
+3. 
+
+'''
--- DualLensAnalyticsUtil.py
+++ DualLensAnalyticsUtil.py
@@ -1,7 +1,11 @@
 #Loading the `config.json` file
+from langchain_openai import ChatOpenAI
 import pandas as pd                # Helpful for working with tabular data like DataFrames
 import matplotlib.pyplot as plt    # Used for Data Visualization / Plots / Graphs
 from langchain.text_splitter import RecursiveCharacterTextSplitter      #  Helpful in splitting the PDF into smaller chunks
+from langchain_community.vectorstores import Chroma    # Vector DataBase
+from langchain_community.document_loaders import PyPDFDirectoryLoader, PyPDFLoader     # Loading a PDF
+
 import yfinance as yf              # Used for gathering stock prices
 import json
 import os
@@ -67,6 +71,7 @@ def fetch_financial_metrics(companies):
         plt.xlabel("Company")
         plt.grid(axis='y')
         #plt.show()
+    return df
 
 def load_ai_initiative_documents():
     # Unzipping the AI Initiatives Documents
@@ -94,3 +99,183 @@ def load_ai_initiative_documents():
     # Total length of all the chunks
     len(ai_initiative_chunks)
     print(f"Total number of document chunks created: {len(ai_initiative_chunks)}")
+    return ai_initiative_chunks
+
+
+def vectorize_ai_initiatives(ai_initiative_chunks):
+    # Defining the 'text-embedding-ada-002' as the embedding model
+    from langchain_openai import OpenAIEmbeddings
+    embedding_model = OpenAIEmbeddings(model="text-embedding-ada-002")
+
+    #  Creating a Vectorstore, storing all the above created chunks using an embedding model
+    vectorstore = Chroma.from_documents(
+        ai_initiative_chunks,
+        embedding_model,
+        collection_name="AI_Initiatives"
+    )
+
+    # Ignore if it gives an error or warning
+
+    # Creating an retriever object which can fetch ten similar results from the vectorstore
+    retriever = vectorstore.as_retriever(
+        search_type="similarity",
+        search_kwargs={"k": 10}
+    )
+    return retriever, vectorstore
+
+# Define RAG function
+def RAG(user_message, qna_system_message, qna_user_message_template, retriever, llm):
+    """
+    Args:
+    user_message: Takes a user input for which the response should be retrieved from the vectorDB.
+    Returns:
+    relevant context as per user query.
+    """
+    relevant_document_chunks = retriever.invoke(user_message)
+    context_list = [d.page_content for d in relevant_document_chunks]
+    context_for_query = ". ".join(context_list)
+
+
+
+    # Combine qna_system_message and qna_user_message_template to create the prompt
+    prompt = f"""[INST]{qna_system_message}\n
+                {'user'}: {qna_user_message_template.format(context=context_for_query, question=user_message)}
+                [/INST]"""
+
+    # Quering the LLM
+    try:
+        response = llm.invoke(prompt)
+
+    except Exception as e:
+        response = f'Sorry, I encountered the following error: \n {e}'
+
+    return response.content
+
+def evaluate_groundedness(evaluation_test_question, qna_system_message, evaluation_user_message_template, retriever, answer, qna_user_message_template, llm):
+    # Writing a question for performing evaluations on the RAG
+    evaluation_test_question = "What are the three projects on which MSFT is working upon?"
+
+    # Building the context for the evaluation test question using the retrieved chunks
+    relevant_document_chunks = retriever.invoke(evaluation_test_question)
+    context_list = [d.page_content for d in relevant_document_chunks]
+    context_for_query = ". ".join(context_list)
+
+    # Default RAG Answer
+    answer = RAG(evaluation_test_question, qna_system_message, qna_user_message_template, retriever, llm)
+    print(answer)
+
+    # Defining user messsage template for evaluation
+    evaluation_user_message_template = """
+    ###Question
+    {question}
+
+    ###Context
+    {context}
+
+    ###Answer
+    {answer}
+    """
+
+    # Writing the system message and the evaluation metrics for checking the groundedness
+    groundedness_rater_system_message = """
+    You are a helpful assistant for evaluating the groundedness of the answer provided to a question based on the given context. Groundedness refers to how well the answer is supported by the information present in the context.
+    """
+
+    # Combining groundedness_rater_system_message + llm_prompt + answer for evaluation
+    groundedness_prompt = f"""[INST]{groundedness_rater_system_message}\n
+                {'user'}: {evaluation_user_message_template.format(context=context_for_query, question=evaluation_test_question, answer=answer)}
+                [/INST]"""
+
+    # Defining a new LLM object
+    groundness_checker = ChatOpenAI(
+        model="gpt-4o-mini",
+        temperature=0,
+        max_tokens=500,
+        top_p=0.95,
+        frequency_penalty=1.2,
+        stop_sequences=['INST']
+    )
+
+    # Using the LLM-as-Judge for evaluating Groundedness
+    groundness_response = groundness_checker.invoke(groundedness_prompt)
+    return groundness_response.content 
+
+def evaluate_relevance(evaluation_test_question, qna_system_message, evaluation_user_message_template, retriever, answer, qna_user_message_template, llm):
+    # Writing a question for performing evaluations on the RAG
+    evaluation_test_question = "What are the three projects on which MSFT is working upon?"
+
+    # Building the context for the evaluation test question using the retrieved chunks
+    relevant_document_chunks = retriever.invoke(evaluation_test_question)
+    context_list = [d.page_content for d in relevant_document_chunks]
+    context_for_query = ". ".join(context_list)
+
+    # Default RAG Answer
+    answer = RAG(evaluation_test_question, qna_system_message, qna_user_message_template, retriever, llm)
+    print(answer)
+
+    # Defining user messsage template for evaluation
+    evaluation_user_message_template = """
+    ###Question
+    {question}
+
+    ###Context
+    {context}
+
+    ###Answer
+    {answer}
+    """
+    # Writing the system message and the evaluation metrics for checking the relevance
+    relevance_rater_system_message = """
+    You are a helpful assistant for evaluating the relevance of the answer provided to a question based on the given context. Relevance refers to how closely the answer addresses the question and utilizes the information present in the context.
+    """
+
+    # Combining relevance_rater_system_message + llm_prompt + answer for evaluation
+    relevance_prompt = f"""[INST]{relevance_rater_system_message}\n
+                {'user'}: {evaluation_user_message_template.format(context=context_for_query, question=evaluation_test_question, answer=answer)}
+                [/INST]"""
+
+    # Defining a new LLM object
+    relevance_checker = ChatOpenAI(
+        model="gpt-4o-mini",
+        temperature=0,
+        max_tokens=500,
+        top_p=0.95,
+        frequency_penalty=1.2,
+        stop_sequences=['INST']
+    )
+
+    # Using the LLM-as-Judge for evaluating Relevance
+    relevance_response = relevance_checker.invoke(relevance_prompt)
+    return relevance_response.content
+
+def score_and_rank_companies(vectorstore, llm, df):
+    # Fetching all the links of the documents
+    len(vectorstore.get()['documents'])
+
+    # Write a system message for instructing the LLM for scoring and ranking the companies
+    system_message = """
+    You are a helpful assistant for evaluating the completeness of the information provided in the context for answering questions related to the AI initiatives of top tech companies. Completeness refers to how well the answer covers all relevant aspects of the question based on the information present in the context.
+    """
+
+    # Write a user message for instructing the LLM for scoring and ranking the companies
+    user_message = f"""
+    Based on the following financial data and AI initiatives of the companies, please provide a comprehensive answer to the question: "Which company is leading in terms of AI initiatives and financial performance, and why?".
+    ---
+    ### 1. Financial Data
+    {df.to_string()}
+
+    ---
+    ### 2. AI Initiatives
+    {vectorstore.get()['documents']}
+    """
+
+    # Formatting the prompt
+    formatted_prompt = f"""[INST]{system_message}\n
+                    {'user'}: {user_message}
+                    [/INST]"""
+
+    # Calling the LLM
+    recommendation = llm.invoke(formatted_prompt)
+    recommendation.content
+
+    return recommendation.content
